c6ed2164d50583f64148a21569fc55c277eb12a8
raechel <odom.raechel@gmail.com>
2019-01-06 11:49:42 -0700

    trash or delete function now deletes single obj on click

--- photo.js
+++ photo.js
@@ -1,5 +1,5 @@
 class Photo {
-  constructor(title, caption, id, file) {
+  constructor(title, caption, id, file, favorite) {
     this.title = title;
     this.caption = caption;
     this.id = id;
@@ -7,16 +7,22 @@ class Photo {
     this.favorite = false;
   }
   saveToStorage(imagesArr) {
+      
     localStorage.setItem('photos', JSON.stringify(imagesArr));
   }
 
-  deleteFromStorage() {
-    // imagesArr.find(function(id) {
-    //   if (id === this.id) {
-    //     localStorage.removeItem(id);
-    //   }
-    // });
-  }
+  static deleteFromStorage(id) {
+    console.log(id)
+    // imagesArr.forEach(function(pic) {
+      if (true) {
+        console.log('green')
+        imagesArr.splice(id, 1);
+        console.log(imagesArr)
+        // localStorage.removeItem(id);
+        localStorage.setItem('photos', JSON.stringify(imagesArr));
+      }
+    }
+  // }
   // updatePhoto() {
   // }
 }
