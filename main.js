658f933495008e451ac59ae5a0bb043eb146ab6d
raechel <odom.raechel@gmail.com>
2019-01-08 23:18:39 -0700

    refactors on a lot of functionality for shortened, better functions

--- main.js
+++ main.js
@@ -4,12 +4,14 @@ var create = document.getElementById('album');
 var input = document.getElementById('file-input');
 var photoGallery = document.querySelector('.photo-section');
 var photoCard = document.getElementById('photo-card-js')
-// var trash = document.getElementById('dlt');
-var favorite = document.getElementById('faveImg')
+var card = document.getElementById('card-footer')
 var title = document.getElementById('title-photo-js');
 var caption = document.getElementById('caption-photo-js');
 var clearStorage = document.getElementById('clear');
 var mainInput = document.querySelector('.main-input');
+var fileUpload = document.querySelector('.upload-file');
+var searchBar = document.getElementById('nav-search-js');
+
 
 
 var imagesArr = JSON.parse(localStorage.getItem('photos')) || [];
@@ -18,60 +20,62 @@ var reader = new FileReader();
 
 //// EVENT LISTENERS ////
 
-window.addEventListener('load', appendPhotos);
+window.addEventListener('load', appendPhotos(imagesArr));
+
+input.addEventListener('change', enableBtn);
 
 create.addEventListener('click', createElement);
 
-clearStorage.addEventListener('click', clearGallery);
+clearStorage.addEventListener('dblclick', clearGallery);
 
 photoGallery.addEventListener('dblclick', updateCard);
 
-mainInput.addEventListener('keyup', enableBtn);
+searchBar.addEventListener('input', searchPhotos);
 
-// trash.addEventListener('click', deletePhoto);
-
-// favorite.addEventListener('click', favoritePhoto);
-
+photoGallery.addEventListener('click', faveOrDelete);
 
 //// FUNCTIONS ////
 
-function appendPhotos() {
-  imagesArr.forEach(function (photo) {
-    photoGallery.innerHTML += `<article data-id="${photo.id}" class="photo-card" id="photo-card-js">
-        <h3 class="card-photo-title" contentEditable="true">${photo.title}</h3>
-        <section class="card-photo-holder"><img src=${photo.file} alt="user-photo" class="gallery-img">
-        <h4 class="card-photo-caption" id="card-photo-cap-js" contentEditable="true">${photo.caption}</h4>
-        </section>
-        <section class="card-footer">
-          <img src="images/delete.svg" class="card-btn" id="dlt" alt="delete photo" onclick="deletePhoto()">
-          <img src=${photo.file ? "images/favorite.svg" : "images/favorite-active.svg"} class="card-btn" id="faveImg" alt="favorite photo" onclick="favoritePhoto(event)">
-        </section>
-      </article>`
+function appendPhotos(array) {
+  imagesArr = [];
+  array.forEach(function (photo) {
+    var newPhoto = new Photo (photo.title, photo.caption, photo.id, photo.file, photo.favorite)
+    imagesArr.push(newPhoto);
   })
+  displayArray(imagesArr), hideHeader();
+}
+
+function displayArray(arr) {
+  photoGallery.innerHTML = '';
+  arr.forEach(photo => addPhoto(photo));
 }
 
 function createElement() {
   if (input.files[0]) {
   reader.readAsDataURL(input.files[0]);
-  reader.onload = addPhoto
+  reader.onload = firstPhoto
   }
 }
 
-function addPhoto(e) {
-  id = Date.now();
+function firstPhoto(e) {
   var newPhoto = new Photo(title.value, caption.value, Date.now(), e.target.result);
-    photoGallery.innerHTML += `<article data-id="${id}" class="photo-card">
-        <h3 class="card-photo-title" contentEditable="true">${title.value}</h3>
-        <section class="card-photo-holder"><img src=${e.target.result} alt="user-photo" class="gallery-img">
-        <h4 class="card-photo-caption" id="card-photo-cap-js" contentEditable="true">${caption.value}</h4>
-        </section>
-        <section class="card-footer">
-          <img src="images/delete.svg" class="card-btn" id="dlt" alt="delete photo" onclick="deletePhoto()">
-          <img src="images/favorite.svg" class="card-btn" id="faveImg" alt="favorite photo" onclick="favoritePhoto(event)">
-        </section>
-      </article>`
     imagesArr.push(newPhoto);
     newPhoto.saveToStorage(imagesArr);
+    displayArray(imagesArr);
+    clearFields();
+}
+
+function addPhoto(photo) {
+    photoGallery.innerHTML += `<article data-id="${photo.id}" class="photo-card">
+        <h3 class="card-photo-title" contentEditable="true">${photo.title}</h3>
+        <section class="card-photo-holder"><img src=${photo.file} alt="user-photo" class="gallery-img">
+        <h4 class="card-photo-caption" id="card-photo-cap-js" contentEditable="true">${photo.caption}</h4>
+        </section>
+        <section class="card-footer">
+          <img src="images/delete.svg" class="card-btn deleted" id="dlt" alt="delete photo">
+          <img src=${photo.favorite ? "images/favorite-active.svg" : "images/favorite.svg"} class="card-btn fave" id="faveImg" alt="favorite photo">
+        </section>
+      </article>`
 }
 
 function updateCard(event) {
@@ -80,21 +84,22 @@ function updateCard(event) {
     document.body.addEventListener('keypress', function (e) {
     var key = e.keyCode;
     if (key === 13) {
-  if (event.target.className === 'card-photo-title') {
-    Photo.updatePhoto(photoId, 'title', event.target.innerText);
-  } else if (event.target.className === 'card-photo-caption') {
-    Photo.updatePhoto(photoCapId, 'caption', event.target.innerText);
-  }
+    if (event.target.className === 'card-photo-title') {
+      Photo.updatePhoto(photoId, 'title', event.target.innerText);
+    } else if (event.target.className === 'card-photo-caption') {
+      Photo.updatePhoto(photoCapId, 'caption', event.target.innerText);
+    }
       event.target.contentEditable = false;
       event.target.blur();
     }
-})
+  })
 }
 
 function deletePhoto(id) {
-    var oldPhoto = imagesArr.find(photo => imagesArr.id === event.target.parentElement.parentElement.dataset.id);
+    var index = imagesArr.findIndex(photo => photo.id === id)
     event.target.parentElement.parentElement.remove();
-    Photo.deleteFromStorage(oldPhoto);
+    imagesArr[index].deleteFromStorage(index, imagesArr);
+    hideHeader();
 }
 
 function favoritePhoto(event) {
@@ -104,7 +109,6 @@ function favoritePhoto(event) {
     Photo.updatePhoto(photoId, 'favorite', true)
   } else {
     event.target.src = 'images/favorite.svg';
-    Photo.favorite === false;
     Photo.updatePhoto(photoId, 'favorite', false)
   }
 }
@@ -117,11 +121,43 @@ function clearGallery(imgesArr) {
 
 function enableBtn() {
   var albumBtn = document.getElementById('album'); 
-   if (title.value.length >= 1 && caption.value.length >= 1) {
+   if (fileUpload.files.length >= 1) {
     album.disabled = false;
-    console.log('enabled')
-  } else if (title.value.length === 0 && caption.value.length === 0) {
-    album.disabled = true;
   }
 }
+
+function hideHeader() {
+  if (imagesArr.length === 0) {
+    photoGallery.insertAdjacentHTML('afterbegin', `<h2>You Need Photos!</h2>`)
+  }
+}
+
+function searchPhotos() {
+  var localStoragePhotos = JSON.parse(localStorage.getItem('photos'));
+  photoGallery.innerHTML = "";
+  var findPhoto = searchBar.value;
+  if (findPhoto[0]) {
+  var filteredPhotos = localStoragePhotos.filter(photo => photo.title.includes(findPhoto) || photo.caption.includes(findPhoto))
+    appendPhotos(filteredPhotos);
+  } else {
+    appendPhotos(imagesArr);
+  }
+}
+
+function clearFields() {
+  title.value = '';
+  caption.value = '';
+  input.value = '';
+}
+
+function faveOrDelete(e) {
+  var cardId = parseInt(e.target.parentElement.parentElement.dataset.id)
+  if (e.target.classList.contains('fave')) {
+    favoritePhoto(event);
+  } else if (e.target.classList.contains('deleted')) {
+    deletePhoto(cardId);
+  }
+}
+
+
 
